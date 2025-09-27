b4a903f55a23e65cc79ad8bda34c76cc826ba43a
FearlessJeny <161617854+FearlessJeny@users.noreply.github.com>
2025-09-27 22:45:19 +0300

    Update carusel.js

--- carusel.js
+++ carusel.js
@@ -1,8 +1,10 @@
 const fleet1 = document.querySelector(".fleet__wrapper-1 .fleet__images");
 const fleet2 = document.querySelector(".fleet__wrapper-2 .fleet__images");
+const fleet3 = document.querySelector(".fleet__wrapper-3 .fleet__images");
 
 const fleet1Content = Array.from(fleet1.children);
 const fleet2Content = Array.from(fleet2.children);
+const fleet3Content = Array.from(fleet3.children);
 
 fleet1Content.forEach((item) => {
     const duplicateNode = item.cloneNode(true);
@@ -14,5 +16,10 @@ fleet2Content.forEach((item) => {
     const duplicateNode = item.cloneNode(true);
     duplicateNode.setAttribute("aria-hidden", true);
     fleet2.appendChild(duplicateNode);
+});
+fleet3Content.forEach((item) => {
+    const duplicateNode = item.cloneNode(true);
+    duplicateNode.setAttribute("aria-hidden", true);
+    fleet3.appendChild(duplicateNode);
 });
 
